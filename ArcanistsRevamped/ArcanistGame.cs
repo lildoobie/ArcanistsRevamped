49d47519e70c9310902ec2ca04370b88a2eb8563
david_bradyw <david_bradyw@yahoo.com>
2018-02-06 09:15:11 -0500

    WE GOT GRAVITY BABY!!

--- ArcanistsRevamped/ArcanistGame.cs
+++ ArcanistsRevamped/ArcanistGame.cs
@@ -3,6 +3,10 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.IO;
 using VelcroPhysics.Collision;
+using VelcroPhysics.Dynamics;
+using VelcroPhysics.Factories;
+using VelcroPhysics.Utilities;
+using VelcroPhysics.Templates;
 
 namespace ArcanistsRevamped
 {
@@ -16,20 +20,36 @@ namespace ArcanistsRevamped
 
         // Declare texture variables.
         private Texture2D textureSky;
-        private Texture2D textureLevel;
+        private Texture2D levelTexture;
         private Texture2D playerTexture;
+
+        // Declare keyboard state variable.
+        private KeyboardState oldKeyState;
+
+        // Declare VelcroPhysics.Dynamics variables.
+        private Body playerBody;
+        private BodyType playerBodyType;
+        private Body levelBody;
+        private World gameWorld;
 
         // Declare position variables.
         private Vector2 playerPosition;
 
-        // Declare physics constants.
-        private const float gravity = 10;
+        // Simple camera controls
+        private Matrix view;
+        private Vector2 cameraPosition;
+        private Vector2 screenCenter;
+        private Vector2 levelOrigin;
+        private Vector2 playerOrigin;
 
         #region Constructor
         public ArcanistGame()
         {
             graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Conent";
+            // Content.RootDirectory = "Content";
+
+            // Create a new world with gravity.
+            gameWorld = new World(new Vector2(0, 9.8f));
         }
         #endregion
 
@@ -59,6 +79,7 @@ namespace ArcanistsRevamped
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            #region FileStream for Content
             FileStream skyStream = new FileStream("Content/sky.jpg", FileMode.Open);
             Texture2D textureSky = Texture2D.FromStream(GraphicsDevice, skyStream);
             skyStream.Dispose();
@@ -67,15 +88,48 @@ namespace ArcanistsRevamped
             FileStream levelStream = new FileStream("Content/level.png", FileMode.Open);
             Texture2D textureLevel = Texture2D.FromStream(GraphicsDevice, levelStream);
             skyStream.Dispose();
-            this.textureLevel = textureLevel;
+            this.levelTexture = textureLevel;
 
             FileStream playerStream = new FileStream("Content/player.png", FileMode.Open);
             Texture2D playerTexture = Texture2D.FromStream(GraphicsDevice, playerStream);
             skyStream.Dispose();
             this.playerTexture = playerTexture;
+            #endregion
 
-            
+            view = Matrix.Identity;
+            cameraPosition = Vector2.Zero;
+            screenCenter = new Vector2(graphics.GraphicsDevice.Viewport.Width / 2f, graphics.GraphicsDevice.Viewport.Height / 2f);
 
+            /* We need XNA to draw the ground and player at the center of the shapes */
+            levelOrigin = new Vector2(textureLevel.Width / 2f, textureLevel.Height / 2f);
+            playerOrigin = new Vector2(playerTexture.Width / 2f, playerTexture.Height / 2f);
+
+            // Velcro Physics expects objects to be scaled to MKS (meters, kilos, seconds)
+            // 1 meters equals 64 pixels here
+            ConvertUnits.SetDisplayUnitToSimUnitRatio(64f);
+
+            /* Player */
+            // Convert screen center from pixels to meters
+            Vector2 playerPosition = ConvertUnits.ToSimUnits(screenCenter) + new Vector2(0, -1.5f);
+
+            // Create the player fixture
+            //playerBody = BodyFactory.CreateBody(gameWorld, playerPosition, 0f, playerBodyType, playerTexture);
+            var playerBodyTemplate = new BodyTemplate();
+            playerBody = BodyFactory.CreateFromTemplate(gameWorld, playerBodyTemplate);
+            playerBody.BodyType = BodyType.Dynamic;
+            // Give it some bounce and friction
+            playerBody.Restitution = 0.3f;
+            playerBody.Friction = 0.5f;
+
+            /* Ground */
+            Vector2 levelPosition = ConvertUnits.ToSimUnits(screenCenter) + new Vector2(0, 1.25f);
+
+            // Create the ground fixture
+            levelBody = BodyFactory.CreateRectangle(gameWorld, ConvertUnits.ToSimUnits(512f), ConvertUnits.ToSimUnits(64f), 1f, levelPosition);
+            levelBody.BodyType = BodyType.Static;
+            // Give it some bounce and friction
+            levelBody.Restitution = 0.3f;
+            levelBody.Friction = 0.5f;
         }
         #endregion
 
@@ -102,18 +156,52 @@ namespace ArcanistsRevamped
                 Exit();
 
             // TODO: Add your update logic here
-            KeyboardState state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.A))
-                playerPosition.X -= 10;
-            if (state.IsKeyDown(Keys.D))
-                playerPosition.X += 10;
-            if (state.IsKeyDown(Keys.W))
-                playerPosition.Y -= 10;
-            if (state.IsKeyDown(Keys.S))
-                playerPosition.Y += 10;
+            System.Diagnostics.Debug.WriteLine(playerBody.Position);
+            System.Diagnostics.Debug.WriteLine(playerPosition);
 
+            HandleKeyboard();
+
+            //We update the world
+            gameWorld.Step((float)gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f);
 
             base.Update(gameTime);
+        }
+        #endregion
+
+        #region HandleKeyboard
+        private void HandleKeyboard()
+        {
+            KeyboardState state = Keyboard.GetState();
+
+            // Move camera
+            if (state.IsKeyDown(Keys.Left))
+                cameraPosition.X += 2.5f;
+            if (state.IsKeyDown(Keys.Right))
+                cameraPosition.X -= 2.5f;
+            if (state.IsKeyDown(Keys.Up))
+                cameraPosition.Y += 2.5f;
+            if (state.IsKeyDown(Keys.Down))
+                cameraPosition.Y -= 2.5f;
+
+            view = Matrix.CreateTranslation(new Vector3(cameraPosition - screenCenter, 0f)) * Matrix.CreateTranslation(new Vector3(screenCenter, 0f));
+
+            // Move the Player
+            if (state.IsKeyDown(Keys.A))
+                playerPosition.X -= 0.25f;
+            if (state.IsKeyDown(Keys.D))
+                playerPosition.X += 0.25f;
+            if (state.IsKeyDown(Keys.W))
+                playerPosition.Y -= 0.25f;
+            if (state.IsKeyDown(Keys.S))
+                playerPosition.Y += 0.25f;
+
+            if (state.IsKeyDown(Keys.Space) && oldKeyState.IsKeyUp(Keys.Space))
+                playerBody.ApplyLinearImpulse(new Vector2(0, -10));
+
+            if (state.IsKeyDown(Keys.Escape))
+                Exit();
+
+            oldKeyState = state;
         }
         #endregion
 
@@ -128,13 +216,12 @@ namespace ArcanistsRevamped
 
             // TODO: Add your drawing code here
 
-            spriteBatch.Begin();
-
-            spriteBatch.Draw(textureSky, new Vector2(0, 0), Color.White);
-            spriteBatch.Draw(textureLevel, new Vector2(0, 0), Color.White);
-            spriteBatch.Draw(playerTexture, playerPosition, Color.White);
-
+            //Draw player and ground
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, view);
+            spriteBatch.Draw(playerTexture, ConvertUnits.ToDisplayUnits(playerBody.Position), null, Color.White, playerBody.Rotation, playerOrigin, 1f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(levelTexture, ConvertUnits.ToDisplayUnits(levelBody.Position), null, Color.White, 0f, levelOrigin, 1f, SpriteEffects.None, 0f);
             spriteBatch.End();
+            
             base.Draw(gameTime);
         }
         #endregion
